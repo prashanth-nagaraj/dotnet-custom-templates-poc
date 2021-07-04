77fd726ec170611f4fda51dd91355362bddcaa44
Prashanth Nagaraj <prashanthnagaraj1989@gmail.com>
2021-07-04 23:24:07 +0530

    Create new template to learn symbol generators

---
+++ templates-root/templates/neu-cloud-native-starter-DDD/DDDClassName.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DDDNamespace
+{
+    public class DDDClassName
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ templates-test/DDDClassName.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace neu-cloud-native-starter-DDD
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+    }
+}
